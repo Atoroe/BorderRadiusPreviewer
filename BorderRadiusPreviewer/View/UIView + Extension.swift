b5a12e0271f859a93d9faa10ab90f675c46311a1
Artiom P <apoluyanovich@gmail.com>
2021-04-21 16:31:58 +0300

    added algorithm for round each corner

--- BorderRadiusPreviewer/View/UIView + Extension.swift
+++ BorderRadiusPreviewer/View/UIView + Extension.swift
@@ -16,6 +16,33 @@ extension UIView {
         self.layer.cornerRadius = radius
     }
     
+    func roundCorners(corners: UIRectCorner, radius: CGFloat) {
+        let path = UIBezierPath(
+            roundedRect: bounds,
+            byRoundingCorners: corners,
+            cornerRadii: CGSize(width: radius, height: radius)
+        )
+        let mask = CAShapeLayer()
+        mask.path = path.cgPath
+        layer.mask = mask
+    }
+    
+    func roundCorners(
+                    topLeft: CGFloat = 0,
+                    topRight: CGFloat = 0,
+                    bottomLeft: CGFloat = 0,
+                    bottomRight: CGFloat = 0
+                    ) {
+        let topLeftRadius = CGSize(width: topLeft, height: topLeft)
+        let topRightRadius = CGSize(width: topRight, height: topRight)
+        let bottomLeftRadius = CGSize(width: bottomLeft, height: bottomLeft)
+        let bottomRightRadius = CGSize(width: bottomRight, height: bottomRight)
+        let maskPath = UIBezierPath(shouldRoundRect: bounds, topLeftRadius: topLeftRadius, topRightRadius: topRightRadius, bottomLeftRadius: bottomLeftRadius, bottomRightRadius: bottomRightRadius)
+        let shape = CAShapeLayer()
+        shape.path = maskPath.cgPath
+        layer.mask = shape
+    }
+    
     func dropShadow(shadowColor: UIColor? = .black) {
         self.layer.masksToBounds = false
         self.layer.shadowColor = shadowColor?.cgColor ?? UIColor.black.cgColor
--- BorderRadiusPreviewer/ViewControllers/BorderRadiusViewController.swift
+++ BorderRadiusPreviewer/ViewControllers/BorderRadiusViewController.swift
@@ -24,7 +24,6 @@ class BorderRadiusViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -33,7 +32,64 @@ class BorderRadiusViewController: UIViewController {
         shapeView.addGradient(startPointColor: startColor, endPointColor: endColor)
         shapeView.roundCorners()
     }
+}
 
-
+//MARK: - TextField and keyBoard extension
+extension BorderRadiusViewController: UITextFieldDelegate {
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        switch textField.tag {
+        case 0:
+            shapeView.roundCorners(corners: .topLeft, radius: 30)
+        case 1:
+            shapeView.roundCorners(corners: .topRight, radius: 30)
+        case 2:
+            shapeView.roundCorners(corners: .bottomRight, radius: 30)
+        default:
+            shapeView.roundCorners(corners: .bottomLeft, radius: 30)
+        }
+        
+        resignFirstResponder()
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        switch textField.tag {
+        case 0:
+            shapeView.roundCorners(corners: .topLeft, radius: 30)
+        case 1:
+            shapeView.roundCorners(corners: .topRight, radius: 30)
+        case 2:
+            shapeView.roundCorners(corners: .bottomRight, radius: 30)
+        default:
+            shapeView.roundCorners(corners: .bottomLeft, radius: 30)
+        }
+        view.endEditing(true)
+        return true
+    }
+    
+    private func registerForKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardWillChange(_:)),
+                                               name: UIResponder.keyboardWillShowNotification,
+                                               object: nil)
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardWillChange(_:)),
+                                               name: UIResponder.keyboardWillHideNotification,
+                                               object: nil)
+    }
+    
+    @objc private func keyboardWillChange(_ notification: NSNotification) {
+        guard let keyboardFrame = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else { return }
+        
+        if notification.name == UIResponder.keyboardWillShowNotification {
+            self.signUpBottomConstraint.constant = keyboardFrame.height
+            self.view.layoutIfNeeded()
+            self.blurryWillChange(statment: true)
+        } else {
+            self.signUpBottomConstraint.constant = self.initialConstraintHeight
+            self.view.layoutIfNeeded()
+            self.blurryWillChange(statment: false)
+            
+        }
+    }
 }
 
